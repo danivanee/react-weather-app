6d80fd7b1ea58deaceb077720917bd6b401db5f4
danivanee <danivanee_170@hotmail.com>
2022-11-04 18:41:40 -0600

    Se agrego nuevo archivo de api.js

---
+++ src/components/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6482d56b38mshcd73b4e6a2278f4p1be9bbjsnc384d7ebd522',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
