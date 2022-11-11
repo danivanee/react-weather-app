8bcb3cbc13301a5ca010c5a2a0fcc3fc66b5ccb1
danivanee <danivanee_170@hotmail.com>
2022-11-10 22:57:58 -0600

    Solucion al error 403 q no permitia acceder al api

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -7,7 +7,7 @@ const CurrentWeather = () => {
                 <p className="city">Belgrade</p>
                 <p className="weather-description">Sunny</p>
             </div>
-            <img alt="weather" className="weather-icon" src=""/>
+            <img alt="weather" className="weather-icon" src="icons/01d.png" />
         </div>
     );
 }
