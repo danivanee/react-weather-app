85ddd76ed0485a2e9c59d77115eb7869b0e9b071
danivanee <danivanee_170@hotmail.com>
2022-11-14 17:57:49 -0600

    se agrego viento, humedad, presion y estilos

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -17,8 +17,20 @@ const CurrentWeather = () => {
             <span className="parameter-label">Details</span>
           </div>
           <div className="parameter-row">
-            <span className="parameter-label">Feels like</span>
+            <span className="parameter-label">Feels like </span>
             <span className="parameter-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind </span>
+            <span className="parameter-value">2 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity </span>
+            <span className="parameter-value">15%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure </span>
+            <span className="parameter-value">15 hPa</span>
           </div>
         </div>
       </div>
