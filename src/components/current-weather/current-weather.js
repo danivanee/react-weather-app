08cc746ee9ee3cfcaf7d46fba2afa014ae2cf447
danivanee <danivanee_170@hotmail.com>
2022-11-14 17:21:57 -0600

    Se agregaron details y sensacion de temperatura

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -10,6 +10,18 @@ const CurrentWeather = () => {
         </div>
         <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
