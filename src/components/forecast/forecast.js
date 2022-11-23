d2396fac8e03c758989daf1549d99b4561e64aec
danivanee <danivanee_170@hotmail.com>
2022-11-22 18:51:22 -0600

    se le dio formato al doc

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -6,12 +6,19 @@ import {
   AccordionItemPanel,
 } from "react-accessible-accordion";
 
-const WEEK_DAYS = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+const WEEK_DAYS = [
+  "Lunes",
+  "Martes",
+  "Miercoles",
+  "Jueves",
+  "Viernes",
+  "Sabado",
+  "Domingo",
+];
 
 const Forecast = ({ data }) => {
   const dainInAWeek = new Date().getDay();
-  
-  
+
   return (
     <>
       <label className="title">Daily</label>
@@ -19,12 +26,16 @@ const Forecast = ({ data }) => {
         {data.list.slice(0, 7).map((item, idx) => (
           <AccordionItem key={idx}>
             <AccordionItemHeading>
-                <AccordionItemButton>
-                    <div className="daily-item">
-                        <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
-                        <label className="day"></label>                    
-                    </div>
-                </AccordionItemButton>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    alt="weather"
+                    className="icon-small"
+                    src={`icons/${item.weather[0].icon}.png`}
+                  />
+                  <label className="day"></label>
+                </div>
+              </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel></AccordionItemPanel>
           </AccordionItem>
